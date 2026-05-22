fae7511ef1dc69edf92f6ab9ad95538afded80a3
VenuSailla <saillavenu012@gmail.com>
2026-05-22 15:28:10 +0530

    fix header warnings

--- src/Mart/Header.jsx
+++ src/Mart/Header.jsx
@@ -4,35 +4,37 @@ import { useSelector } from "react-redux"
 
 
 export const Header = () => {
- 
+
   let cartIcon = useSelector(state => state.cartIcon)
   return (
     <div className="Container">
-      <nav class="navbar navbar-expand-sm navbar-dark bg-light ">
-        <h2><i class="bi bi-bag-fill ms-5"></i></h2>
-        <div class="container-fluid text-dark">
-          <a class="navbar-brand text-dark" href="javascript:void(0)"><h2>Mart</h2></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-light ">
+        <h2><i className="bi bi-bag-fill ms-5"></i></h2>
+        <div className="container-fluid text-dark">
+          <Link className="navbar-brand text-dark" to="/">
+            <h2>Mart</h2>
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <form class='me-5'>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <Link class="nav-link active text-dark" aria-current="page" to="/"><h5>Home</h5></Link>
+          <form className='me-5'>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link active text-dark" aria-current="page" to="/"><h5>Home</h5></Link>
                 </li>
 
-                <li class="nav-item">
-                  <Link class="nav-link active text-dark" aria-current="page" to="/Shop"><h5>Shop</h5></Link>
+                <li className="nav-item">
+                  <Link className="nav-link active text-dark" aria-current="page" to="/Shop"><h5>Shop</h5></Link>
                 </li>
-                <li class="nav-item">
-                  <Link class="nav-link active text-dark" aria-current="page" to="/Cart"><h5>Cart</h5></Link>
+                <li className="nav-item">
+                  <Link className="nav-link active text-dark" aria-current="page" to="/Cart"><h5>Cart</h5></Link>
                 </li>
-                <li class="nav-item">
-                  <Link class="nav-link active text-dark" aria-current="page" to="/SignUp"><h5><i class="bi bi-person-fill"></i></h5></Link>
+                <li className="nav-item">
+                  <Link className="nav-link active text-dark" aria-current="page" to="/SignUp"><h5><i class="bi bi-person-fill"></i></h5></Link>
                 </li>
-                <li class="nav-item">
-                  <Link class="nav-link active text-dark font" aria-current="page" to="/CartIcon"><h5><i class="bi bi-cart-fill "></i> <sup className='bg-danger p-1 rounded-circle'>{cartIcon.length}</sup></h5></Link>
+                <li className="nav-item">
+                  <Link className="nav-link active text-dark font" aria-current="page" to="/CartIcon"><h5><i class="bi bi-cart-fill "></i> <sup className='bg-danger p-1 rounded-circle'>{cartIcon.length}</sup></h5></Link>
                 </li>
               </ul>
             </div>
@@ -40,9 +42,9 @@ export const Header = () => {
         </div>
       </nav >
 
-    
-  
-</div>
+
+
+    </div>
 
   )
 }
